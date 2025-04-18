85dfbbb7cd5fe723872281aaaefe857e4831005d
crayoninvok <dathariqf@gmail.com>
2025-04-18 16:41:58 +0700

    try

--- dist/config/prisma.js
+++ dist/config/prisma.js
@@ -1,10 +1,15 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-// Create a singleton Prisma instance
-const prisma = new client_1.PrismaClient({
+exports.prisma = void 0;
+// src/lib/prisma.ts (recommended path)
+const client_1 = require("../../prisma/generated/client");
+const globalForPrisma = globalThis;
+exports.prisma = (_a = globalForPrisma.prisma) !== null && _a !== void 0 ? _a : new client_1.PrismaClient({
     log: process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
 });
-exports.default = prisma;
+if (process.env.NODE_ENV !== "production")
+    globalForPrisma.prisma = exports.prisma;
+exports.default = exports.prisma;
